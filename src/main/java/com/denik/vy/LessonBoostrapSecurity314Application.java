50572a3b4a18cdfb014650e30fcc2ab608727777
Freeman <hhaid@bk.ru>
2022-05-29 14:05:56 +0300

    start

---
+++ src/main/java/com/denik/vy/LessonBoostrapSecurity314Application.java
@@ -0,0 +1,13 @@
+package com.denik.vy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonBoostrapSecurity314Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessonBoostrapSecurity314Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/denik/vy/LessonBoostrapSecurity314ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denik.vy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonBoostrapSecurity314ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
